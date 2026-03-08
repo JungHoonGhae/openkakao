c5f22d1a38099fa2c99906f5c817d84f225322bf
Lucas <42439321+JungHoonGhae@users.noreply.github.com>
2026-03-08 13:56:01 +0900

    docs: align site with official fumadocs template

--- website/app/(home)/layout.tsx
+++ website/app/(home)/layout.tsx
@@ -2,5 +2,5 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions('en', 'home')}>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
 }
